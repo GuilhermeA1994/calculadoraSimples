e5901f76e8fec92375612a303e2370c40fa87dcd
GuilhermeA1994 <162219114+GuilhermeA1994@users.noreply.github.com>
2024-09-23 17:05:24 -0300

    Initial commit

---
+++ src/main/java/com/mycompany/calculadorasimples/CalculadoraSimples.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadorasimples;
+
+/**
+ *
+ * @author Gabrielen
+ */
+public class CalculadoraSimples {
+
+    public static void main(String[] args) {
+        telaPrincipal form = new telaPrincipal();
+        form.setVisible(true);
+    }
+}
